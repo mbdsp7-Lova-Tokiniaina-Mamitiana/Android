e004846f19e0bd70456f75dc83a66a9c03db4a2a
Tokiniaina Hervé Andrianarison <tokiniainaherve.andrianarison@gmail.com>
2020-09-19 17:54:15 +0300

    notification

---
+++ app/src/main/java/com/example/etu000603_android/service/FirebaseService.java
@@ -0,0 +1,95 @@
+package com.example.etu000603_android.service;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.IBinder;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+import java.util.Random;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+public class FirebaseService extends FirebaseMessagingService {
+    public FirebaseService() {
+    }
+
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        if(remoteMessage.getData().isEmpty()){
+            showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+        }else{
+            showNotification(remoteMessage.getData());
+        }
+    }
+
+    private void showNotification(Map<String,String> data){
+        String title = data.get("title").toString();
+        String body = data.get("body").toString();
+
+        String NOTIFICATION_CHANNEL_ID = "com.example.connectionfirebase.services";
+
+        NotificationManager notificationManager =(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,"Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Code Sphere");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info");
+
+        assert notificationManager != null;
+        notificationManager.notify(new Random().nextInt(),notificationBuilder.build());
+
+    }
+
+    private void showNotification(String title,String body){
+        String NOTIFICATION_CHANNEL_ID = "com.example.connectionfirebase.services";
+        NotificationManager notificationManager =(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,"Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Code Sphere");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info");
+
+        assert notificationManager != null;
+        notificationManager.notify(new Random().nextInt(),notificationBuilder.build());
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        System.out.println("token:"+s);
+
+    }
+}
